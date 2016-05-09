87c55580ffb17ca42e2e9cecedee6f88603d4633
Jonathan Morales <jam072093@gmail.com>
2016-05-09 14:24:51 -0400

    Added empty drop view

--- app/src/main/java/com/example/jonathan/bucketdrops/MainActivity.java
+++ app/src/main/java/com/example/jonathan/bucketdrops/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.jonathan.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity  {
+
+    Toolbar mToolbar;
+    Button mBtnAdd;
+
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mBtnAdd = (Button) findViewById(R.id.add_btn);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
+        initBackgroundImage();
     }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(background);
+    }
+
 }
